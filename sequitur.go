403e270bea1e7ca2e78e8c1c2cb845bfb72bb977
dogacel <dogacel@gmail.com>
2018-11-06 12:17:27 +0300

    Completed sequitur. Need to debug.

--- sequitur.go
+++ sequitur.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
 func tokenize(in string) (out string) {
@@ -17,11 +19,17 @@ func tokenize(in string) (out string) {
 }
 
 func nextToken(in string, index int) (string, int) {
-	if index+2 > len(in) {
+	if index+1 >= len(in) {
+		if index+1 == len(in) {
+			return string(in[index]), -1
+		}
 		return "", -1
 	}
 
 	if in[index] == '^' {
+		if len(in) == index+2 {
+			return in[index : index+2], -1
+		}
 		if in[index+2] == '^' {
 			return in[index : index+4], index + 2
 		}
@@ -36,16 +44,23 @@ func nextToken(in string, index int) (string, int) {
 }
 
 func main() {
-	var input string
-	fmt.Scanln(&input)
+
+	dat, err := ioutil.ReadFile("in.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	input := string(dat)
+	fmt.Println(input)
 
 	input = tokenize(input)
 
 	var token string
 	index := 0
 
-	keyIndex := rune(0)
+	keyIndex := rune(48)
 	dict := make(map[string]struct{})
+	fullDict := make(map[string]string)
 
 	token, index = nextToken(input, index)
 
@@ -56,16 +71,49 @@ func main() {
 			delete(dict, token)
 			newKey := "^" + string(keyIndex)
 			dict[newKey] = struct{}{}
+			keyIndex++
+			if keyIndex == '^' {
+				keyIndex++
+			}
 
 			// Change tokens to newKey
+			subToken, subIndex := "", 0
+			outstring := ""
 
+			for subIndex != -1 {
+				subToken, subIndex = nextToken(input, subIndex)
+				if subToken != "" {
+					if subToken == token {
+						outstring += newKey
+						if token[0] == '^' {
+							subIndex += 2
+						} else {
+							subIndex++
+						}
+					} else {
+						if subToken[0] == '^' {
+							outstring += string(subToken[0:2])
+						} else {
+							outstring += string(subToken[0])
+						}
+					}
+				}
+			}
+
+			input = outstring
+			dict = make(map[string]struct{})
+			fullDict[newKey] = token
 			index = 0
 		} else {
 			dict[token] = struct{}{}
 		}
 
-		// fmt.Println(token + " : " + strconv.Itoa(index))
+		//fmt.Println(token + " : " + strconv.Itoa(index))
 		token, index = nextToken(input, index)
 	}
 
+	fmt.Println("Output: " + input)
+	for key, val := range fullDict {
+		fmt.Println(key + " ~ " + strings.Replace(val, "\n", "⏎", -1))
+	}
 }
